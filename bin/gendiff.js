7856b0da7f17e2e0eb95d797411e5e3283b36ef7
Alexandra Vysochina <puzinka_77@mail.ru>
2022-06-24 17:28:07 +0300

    step 2. added arguments and option to a gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,13 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1');
+
+program
+    .argument('<fileputh1>')
+    .argument('<fileputh2>')
+    .option('-f, --format <type>', 'output format')
  
 program.parse();
